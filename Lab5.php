12a6a6d711e75c7456d4b816c164c4cd3141d60b
Шатура Евгений <57914563+udogaL2@users.noreply.github.com>
2023-11-20 22:32:53 +0200

    Lab5: конец

--- Lab5.php
+++ Lab5.php
@@ -2,23 +2,17 @@
 
 class User
 {
-	private int $id;
 	private string $name;
 	private string $login;
 	private string $password;
 
-	public function __construct(int $id, string $name, string $login, string $password)
+	public function __construct(string $name, string $login, string $password)
 	{
-		$this->id = $id;
 		$this->name = $name;
 		$this->login = $login;
 		$this->password = $password;
 	}
 
-	public function getId(): int
-	{
-		return $this->id;
-	}
 
 	public function getName(): string
 	{
@@ -53,7 +47,7 @@ class User
 
 interface UserManagerInterface
 {
-	public static function signIn(User $user): bool;
+	public static function signIn(string $login, string $password): bool;
 
 	public static function singUp(User $user): bool;
 
@@ -64,72 +58,79 @@ interface UserManagerInterface
 
 interface DataRepositoryInterface
 {
-	public static function get(): array;
+	public function get(): array;
 
-	public static function add(User $item): bool;
+	public function add(User $user): bool;
 
-	public static function remove(User $item): bool;
+	public function remove(User $user): bool;
 
-	public static function update(User $item): bool;
+	public function update(User $user): bool;
 }
 
-interface UserRepositoryInterface
+interface UserRepositoryInterface extends DataRepositoryInterface
 {
-	public function getById(int $id): User;
+	public function getById(int $id): ?User;
 
-	public function getByName(string $name): User;
+	public function getByName(string $name): ?User;
 }
 
 class DataRepository implements DataRepositoryInterface
 {
-	private static string $DIRECTORY = __DIR__  . '/data/';
-	private static array $repository;
+	protected string $DIRECTORY = __DIR__  . '\\data\\';
+	protected array $repository = [];
 
-	public static function get(): array
+	public function get(): array
 	{
-		$files =  array_diff(scandir(self::$DIRECTORY), array('..', '.'));
+		$this->checkDirection();
+
+		$files =  array_diff(scandir($this->DIRECTORY), array('..', '.'));
 
 		foreach ($files as $file)
 		{
 			if (str_contains($file, '.txt'))
 			{
-				$handle = file_get_contents(self::$DIRECTORY . $file);
+				$handle = file_get_contents($this->DIRECTORY . $file);
 
+				/* @var User $data */
 				$data = unserialize($handle, ['allowed_classes' => ['User']]);
 
-				if ($data)
+				if ($data && empty($this->repository[$data->getLogin()]))
 				{
-					self::$repository[] = $data;
+					$this->repository[$data->getLogin()] = $data;
 				}
 			}
 		}
 
-		return self::$repository;
+		return $this->repository;
 	}
 
-	public static function add(User $item): bool
+	public function add(User $user): bool
 	{
-		$file = self::$DIRECTORY . $item->getName();
+		$this->checkDirection();
+
+		$file = $this->DIRECTORY . $user->getName() . '.txt';
 
 		if (file_exists($file))
 		{
 			return false;
 		}
 
-		if(!file_put_contents($file, serialize($file)))
+		if(!file_put_contents($file, serialize($user)))
 		{
 			return false;
 		}
 
 
-		self::$repository[$item->getName()] = $item;
+		$this->repository[$user->getLogin()] = $user;
 
 		return true;
 	}
 
-	public static function remove(User $item): bool
+	public function remove(User $user): bool
 	{
-		$file = self::$DIRECTORY . $item->getName();
+		$this->checkDirection();
+
+		$file = $this->DIRECTORY . $user->getName() . '.txt';
 
 		if (!file_exists($file))
 		{
@@ -141,40 +142,146 @@ class DataRepository implements DataRepositoryInterface
 			return false;
 		}
 
-		unset(self::$repository[$item->getName()]);
+		unset($this->repository[$user->getLogin()]);
 
 		return true;
 	}
 
-	public static function update(User $item): bool
+	public function update(User $user): bool
 	{
-		return $this->remove($item) && $this->add($item);
+		return $this->remove($user) && $this->add($user);
+	}
+
+	protected function checkDirection(): void
+	{
+		if (!file_exists($this->DIRECTORY))
+		{
+			mkdir($this->DIRECTORY);
+		}
 	}
 }
 
-class UserManagerBase implements UserManagerInterface
+class UserRepository extends DataRepository implements UserRepositoryInterface
 {
-	public static function signIn(User $user): bool
-	{
+	protected string $DIRECTORY = __DIR__  . '\\cookie\\';
+	protected array $repository = [];
 
-		return true;
+	public function getById(int $id): ?User
+	{
+		return $this->repository[$id] ?? null;
+	}
+
+	public function getByName(string $name): ?User
+	{
+		/* @var User $user*/
+		foreach ($this->repository as $user)
+		{
+			if ($user->getName() === $name)
+			{
+				return $user;
+			}
+		}
+
+		return null;
+	}
+}
+
+class UserManager implements UserManagerInterface
+{
+	public static ?User $currentUser = null;
+
+	public static function signIn(string $login, ?string $password = null): bool
+	{
+		$data = DataManager::getDataRepository()->get();
+
+		if (empty($data[$login]))
+		{
+			return false;
+		}
+
+		/* @var User $user*/
+		$user = $data[$login];
+
+		if (isset(DataManager::getUserRepository()->get()[$login]))
+		{
+			self::$currentUser = $data[$login];
+
+			return true;
+		}
+
+		if ($user->getPassword() !== $password)
+		{
+			return false;
+		}
+
+		self::$currentUser = $data[$login];
+
+		return DataManager::getUserRepository()->add(self::$currentUser);
 	}
 
 	public static function signOut(): bool
 	{
-		// TODO: Implement signOut() method.
-		return true;
+		if (!self::isAuthorized())
+		{
+			return false;
+		}
+
+		$result = DataManager::getUserRepository()->remove(self::$currentUser);
+		self::$currentUser = null;
+
+		return $result;
 	}
 
 	public static function isAuthorized(): bool
 	{
-		// TODO: Implement isAuthorized() method.
-		return true;
+		return self::$currentUser !== null;
 	}
 
 	public static function singUp(User $user): bool
 	{
-		return DataRepository::add($user);
+		$data = DataManager::getDataRepository()->get();
+
+		if (isset($data[$user->getLogin()]))
+		{
+			return false;
+		}
+
+		return DataManager::getDataRepository()->add($user);
 	}
 }
 
+class DataManager
+{
+	private static ?DataRepository $dataRepository;
+	private static ?UserRepository $userRepository;
+
+	public static function getUserRepository(): UserRepository
+	{
+		if (empty(self::$userRepository))
+		{
+			self::$userRepository = new UserRepository();
+		}
+
+		return self::$userRepository;
+	}
+
+	public static function getDataRepository(): DataRepository
+	{
+		if (empty(self::$dataRepository))
+		{
+			self::$dataRepository = new DataRepository();
+		}
+
+		return self::$dataRepository;
+	}
+}
+
+function test1(): void
+{
+	$login = 'bob1';
+
+	var_dump(UserManager::signIn($login));
+	var_dump(UserManager::signOut());
+}
+
+test1();
