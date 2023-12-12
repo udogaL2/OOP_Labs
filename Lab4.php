1ac8627533f2cc2dd052aae4cea2aedb4ea87766
Шатура Евгений <57914563+udogaL2@users.noreply.github.com>
2023-12-12 20:34:04 +0200

    Lab4: добавлен класс клавиши для назначения функционала

--- Lab4.php
+++ Lab4.php
@@ -1,178 +1,72 @@
 <?php
 
-class VirtualBoard extends VirtualKeyList implements VirtualBoardInterface
+class VirtualBoard
 {
-	public static function pressButton(string $button): void
-	{
-		$method = 'getButton' . $button;
+	/**
+	 * @var VirtualButton[]
+	 */
+	private array $buttonList;
 
-		if (
-			self::isKeyAvailbale($button)
-			&& method_exists(VirtualBoard::class, $method)
-		)
+	private static ?VirtualBoard $instance = null;
+
+	static public function getInstance(): VirtualBoard
+	{
+		if (!isset(self::$instance))
 		{
-			self::press(call_user_func('VirtualBoard::' . $method));
+			self::$instance = new VirtualBoard();
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct()
+	{
+		$this->buttonList = [];
+	}
+
+	public function pressButton(string $button): void
+	{
+		if (isset($this->buttonList[$button]))
+		{
+			$this->buttonList[$button]->execute();
 		}
 	}
 
-	private static function press(string $command): void
+	public function addButton(VirtualButton $button): void
 	{
-		echo $command . "\n";
+		$this->buttonList[$button->getOriginalButton()] = $button;
 	}
 }
 
-class VirtualKeyList
+class VirtualButton
 {
-	static protected array $availableKeyList = [
-		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O',
-		'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
-		'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M',
-	];
+	protected string $originalButton;
+	protected $executeFunction;
 
-	protected static function getButtonQ(): string
+	public function __construct(string $originalButton, callable $executeFunction)
 	{
-		return 'A';
+		$this->originalButton = $originalButton;
+		$this->executeFunction = $executeFunction;
 	}
 
-	protected static function getButtonW(): string
+	public function getOriginalButton(): string
 	{
-		return 'S';
+		return $this->originalButton;
 	}
 
-	protected static function getButtonE(): string
+	public function execute():void
 	{
-		return 'D';
-	}
-
-	protected static function getButtonR(): string
-	{
-		return 'F';
-	}
-
-	protected static function getButtonT(): string
-	{
-		return 'G';
-	}
-
-	protected static function getButtonY(): string
-	{
-		return 'H';
-	}
-
-	protected static function getButtonU(): string
-	{
-		return 'J';
-	}
-
-	protected static function getButtonI(): string
-	{
-		return 'K';
-	}
-
-	protected static function getButtonO(): string
-	{
-		return 'L';
-	}
-
-	protected static function getButtonP(): string
-	{
-		return 'Z';
-	}
-
-	protected static function getButtonA(): string
-	{
-		return 'X';
-	}
-
-	protected static function getButtonS(): string
-	{
-		return 'C';
-	}
-
-	protected static function getButtonD(): string
-	{
-		return 'V';
-	}
-
-	protected static function getButtonF(): string
-	{
-		return 'B';
-	}
-
-	protected static function getButtonG(): string
-	{
-		return 'N';
-	}
-
-	protected static function getButtonH(): string
-	{
-		return 'M';
-	}
-
-	protected static function getButtonJ(): string
-	{
-		return 'Q';
-	}
-
-	protected static function getButtonK(): string
-	{
-		return 'W';
-	}
-
-	protected static function getButtonL(): string
-	{
-		return 'E';
-	}
-
-	protected static function getButtonZ(): string
-	{
-		return 'R';
-	}
-
-	protected static function getButtonX(): string
-	{
-		return 'T';
-	}
-
-	protected static function getButtonC(): string
-	{
-		return 'Y';
-	}
-
-	protected static function getButtonV(): string
-	{
-		return 'U';
-	}
-
-	protected static function getButtonB(): string
-	{
-		return 'I';
-	}
-
-	protected static function getButtonN(): string
-	{
-		return 'O';
-	}
-
-	protected static function getButtonM(): string
-	{
-		return 'P';
-	}
-
-	public static function isKeyAvailbale(string $button): bool
-	{
-		return in_array($button, self::$availableKeyList);
+		($this->executeFunction)();
 	}
 }
-
-interface VirtualBoardInterface
-{
-	public static function pressButton(string $button);
-}
-
 
 function start(): void
 {
+	VirtualBoard::getInstance()->addButton(new VirtualButton('a', function () { echo "Имитация замены клавиши a\n"; }));
+	VirtualBoard::getInstance()->addButton(new VirtualButton('A', function () { echo "Имитация замены клавиши shift + a\n"; }));
+	VirtualBoard::getInstance()->addButton(new VirtualButton('e', function () { echo "Имитация замены клавиши e\n"; }));
+	VirtualBoard::getInstance()->addButton(new VirtualButton('E', function () { exec('explorer'); }));
+
 	while (true)
 	{
 		$command = readline();
@@ -182,7 +76,7 @@ function start(): void
 			break;
 		}
 
-		VirtualBoard::pressButton($command);
+		VirtualBoard::getInstance()->pressButton($command);
 	}
 }
 
